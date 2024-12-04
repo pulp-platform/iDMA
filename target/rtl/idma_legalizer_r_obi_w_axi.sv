894bcc5a877db9100bd0c51b331b40e66f3624d0
github-ci <github-ci@iis.ee.ethz.ch>
2024-12-04 10:41:38 +0000

    Multistream register frontend: Fix number of protocol bits and location
    Co-authored-by: Georg Rutishauser <georgr@iis.ee.ethz.ch>
    
    -----
    
    Deployed from ec2d84afeededa9d26cf5af23352b0f4c574b0bf

---
+++ target/rtl/idma_legalizer_r_obi_w_axi.sv
@@ -0,0 +1,420 @@
+// Copyright 2023 ETH Zurich and University of Bologna.
+// Solderpad Hardware License, Version 0.51, see LICENSE for details.
+// SPDX-License-Identifier: SHL-0.51
+
+// Authors:
+// - Thomas Benz <tbenz@iis.ee.ethz.ch>
+// - Tobias Senti <tsenti@ethz.ch>
+
+`include "common_cells/registers.svh"
+`include "common_cells/assertions.svh"
+`include "idma/guard.svh"
+
+/// Legalizes a generic 1D transfer according to the rules given by the
+/// used protocol.
+module idma_legalizer_r_obi_w_axi #(
+    /// Should both data shifts be done before the dataflow element?
+    /// If this is enabled, then the data inserted into the dataflow element
+    /// will no longer be word aligned, but only a single shifter is needed
+    parameter bit          CombinedShifter = 1'b0,
+    /// Data width
+    parameter int unsigned DataWidth       = 32'd16,
+    /// Address width
+    parameter int unsigned AddrWidth       = 32'd24,
+    /// 1D iDMA request type:
+    /// - `length`: the length of the transfer in bytes
+    /// - `*_addr`: the source / target byte addresses of the transfer
+    /// - `opt`: the options field
+    parameter type idma_req_t        = logic,
+    /// Read request type
+    parameter type idma_r_req_t      = logic,
+    /// Write request type
+    parameter type idma_w_req_t      = logic,
+    /// Mutable transfer type
+    parameter type idma_mut_tf_t     = logic,
+    /// Mutable options type
+    parameter type idma_mut_tf_opt_t = logic
+)(
+    /// Clock
+    input  logic clk_i,
+    /// Asynchronous reset, active low
+    input  logic rst_ni,
+
+    /// 1D request
+    input  idma_req_t req_i,
+    /// 1D request valid
+    input  logic valid_i,
+    /// 1D request ready
+    output logic ready_o,
+
+    /// Read request; contains datapath and meta information
+    output idma_r_req_t r_req_o,
+    /// Read request valid
+    output logic r_valid_o,
+    /// Read request ready
+    input  logic r_ready_i,
+
+    /// Write request; contains datapath and meta information
+    output idma_w_req_t w_req_o,
+    /// Write request valid
+    output logic w_valid_o,
+    /// Write request ready
+    input  logic w_ready_i,
+
+    /// Invalidate the current burst transfer, stops emission of requests
+    input  logic flush_i,
+    /// Kill the active 1D transfer; reload a new transfer
+    input  logic kill_i,
+
+    /// Read machine of the legalizer is busy
+    output logic r_busy_o,
+    /// Write machine of the legalizer is busy
+    output logic w_busy_o
+);
+    function int unsigned max_size(input int unsigned a, b);
+        return a > b ? a : b;
+    endfunction
+
+    /// Stobe width
+    localparam int unsigned StrbWidth     = DataWidth / 8;
+    /// Offset width
+    localparam int unsigned OffsetWidth   = $clog2(StrbWidth);
+    /// The size of a page in byte
+    localparam int unsigned PageSize      = max_size(256 * StrbWidth > 4096 ? 4096 : 256 * StrbWidth, StrbWidth);
+    /// The width of page offset byte addresses
+    localparam int unsigned PageAddrWidth = $clog2(PageSize);
+
+    /// Offset type
+    typedef logic [  OffsetWidth-1:0] offset_t;
+    /// Address type
+    typedef logic [    AddrWidth-1:0] addr_t;
+    /// Page address type
+    typedef logic [PageAddrWidth-1:0] page_addr_t;
+    /// Page length type
+    typedef logic [  PageAddrWidth:0] page_len_t;
+
+
+    // state: internally hold one transfer, this is mutated
+    idma_mut_tf_t     r_tf_d,   r_tf_q;
+    idma_mut_tf_t     w_tf_d,   w_tf_q;
+    idma_mut_tf_opt_t opt_tf_d, opt_tf_q;
+
+    // enable signals for next mutable transfer storage
+    logic r_tf_ena;
+    logic w_tf_ena;
+
+    // page boundaries
+    page_len_t r_page_num_bytes_to_pb;
+    page_len_t r_num_bytes_to_pb;
+    page_len_t w_page_num_bytes_to_pb;
+    page_len_t w_num_bytes_to_pb;
+    page_len_t c_num_bytes_to_pb;
+
+    // read process
+    page_len_t r_num_bytes_possible;
+    page_len_t r_num_bytes;
+    offset_t   r_addr_offset;
+    logic      r_done;
+
+    // write process
+    page_len_t w_num_bytes_possible;
+    page_len_t w_num_bytes;
+    offset_t   w_addr_offset;
+    logic      w_done;
+
+
+    //--------------------------------------
+    // read boundary check
+    //--------------------------------------
+    idma_legalizer_page_splitter #(
+        .OffsetWidth   ( OffsetWidth   ),
+        .PageAddrWidth ( PageAddrWidth ),
+        .addr_t        ( addr_t        ),
+        .page_len_t    ( page_len_t    ),
+        .page_addr_t   ( page_addr_t   )
+    ) i_read_page_splitter (
+        .not_bursting_i    ( 1'b1 ),
+
+        .reduce_len_i      ( opt_tf_q.src_reduce_len ),
+        .max_llen_i        ( opt_tf_q.src_max_llen   ),
+
+        .addr_i            ( r_tf_q.addr             ),
+        .num_bytes_to_pb_o ( r_page_num_bytes_to_pb  )
+    );
+
+    assign r_num_bytes_to_pb = r_page_num_bytes_to_pb;
+
+    //--------------------------------------
+    // write boundary check
+    //--------------------------------------
+    idma_legalizer_page_splitter #(
+        .OffsetWidth   ( OffsetWidth   ),
+        .PageAddrWidth ( PageAddrWidth ),
+        .addr_t        ( addr_t        ),
+        .page_len_t    ( page_len_t    ),
+        .page_addr_t   ( page_addr_t   )
+    ) i_write_page_splitter (
+        .not_bursting_i    ( opt_tf_q.dst_protocol inside { idma_pkg::OBI} ),
+
+        .reduce_len_i      ( opt_tf_q.dst_reduce_len ),
+        .max_llen_i        ( opt_tf_q.dst_max_llen   ),
+
+        .addr_i            ( w_tf_q.addr             ),
+        .num_bytes_to_pb_o ( w_page_num_bytes_to_pb  )
+    );
+
+    assign w_num_bytes_to_pb = w_page_num_bytes_to_pb;
+
+    //--------------------------------------
+    // page boundary check
+    //--------------------------------------
+    // how many transfers are remaining when concerning both r/w pages?
+    // take the boundary that is closer
+    assign c_num_bytes_to_pb = (r_num_bytes_to_pb > w_num_bytes_to_pb) ?
+                                w_num_bytes_to_pb : r_num_bytes_to_pb;
+
+
+    //--------------------------------------
+    // Synchronized R/W process
+    //--------------------------------------
+    always_comb begin : proc_num_bytes_possible
+        // Default: Coupled
+        r_num_bytes_possible = c_num_bytes_to_pb;
+        w_num_bytes_possible = c_num_bytes_to_pb;
+
+        if (opt_tf_q.decouple_rw
+            || (opt_tf_q.src_protocol inside { idma_pkg::OBI })) begin
+            r_num_bytes_possible = r_num_bytes_to_pb;
+            w_num_bytes_possible = w_num_bytes_to_pb;
+        end
+    end
+
+    assign r_addr_offset = r_tf_q.addr[OffsetWidth-1:0];
+    assign w_addr_offset = w_tf_q.addr[OffsetWidth-1:0];
+
+    // legalization process -> read and write is coupled together
+    always_comb begin : proc_read_write_transaction
+
+        // default: keep state
+        r_tf_d   = r_tf_q;
+        w_tf_d   = w_tf_q;
+        opt_tf_d = opt_tf_q;
+
+        // default: not done
+        r_done = 1'b0;
+        w_done = 1'b0;
+
+        //--------------------------------------
+        // Legalize read transaction
+        //--------------------------------------
+        // more bytes remaining than we can read
+        if (r_tf_q.length > r_num_bytes_possible) begin
+            r_num_bytes = r_num_bytes_possible;
+            // calculate remainder
+            r_tf_d.length = r_tf_q.length - r_num_bytes_possible;
+            // next address
+            r_tf_d.addr = r_tf_q.addr + r_num_bytes;
+
+        // remaining bytes fit in one burst
+        end else begin
+            r_num_bytes = r_tf_q.length[PageAddrWidth:0];
+            // finished
+            r_tf_d.valid = 1'b0;
+            r_done = 1'b1;
+        end
+
+        //--------------------------------------
+        // Legalize write transaction
+        //--------------------------------------
+        // more bytes remaining than we can write
+        if (w_tf_q.length > w_num_bytes_possible) begin
+            w_num_bytes = w_num_bytes_possible;
+            // calculate remainder
+            w_tf_d.length = w_tf_q.length - w_num_bytes_possible;
+            // next address
+            w_tf_d.addr = w_tf_q.addr + w_num_bytes;
+
+        // remaining bytes fit in one burst
+        end else begin
+            w_num_bytes = w_tf_q.length[PageAddrWidth:0];
+            // finished
+            w_tf_d.valid = 1'b0;
+            w_done = 1'b1;
+        end
+
+        //--------------------------------------
+        // Kill
+        //--------------------------------------
+        if (kill_i) begin
+            // kill the current state
+            r_tf_d = '0;
+            w_tf_d = '0;
+            r_done = 1'b1;
+            w_done = 1'b1;
+        end
+
+        //--------------------------------------
+        // Refill
+        //--------------------------------------
+        // new request is taken in if both r and w machines are ready.
+        if (ready_o & valid_i) begin
+
+            // load all three mutable objects (source, destination, option)
+            // source or read
+            r_tf_d = '{
+                length: req_i.length,
+                addr:   req_i.src_addr,
+                valid:   1'b1,
+                base_addr: req_i.src_addr
+            };
+            // destination or write
+            w_tf_d = '{
+                length: req_i.length,
+                addr:   req_i.dst_addr,
+                valid:   1'b1,
+                base_addr: req_i.dst_addr
+            };
+            // options
+            opt_tf_d = '{
+                src_protocol:   req_i.opt.src_protocol,
+                dst_protocol:   req_i.opt.dst_protocol,
+                read_shift:     '0,
+                write_shift:    '0,
+                decouple_rw:    req_i.opt.beo.decouple_rw,
+                decouple_aw:    req_i.opt.beo.decouple_aw,
+                src_max_llen:   req_i.opt.beo.src_max_llen,
+                dst_max_llen:   req_i.opt.beo.dst_max_llen,
+                src_reduce_len: req_i.opt.beo.src_reduce_len,
+                dst_reduce_len: req_i.opt.beo.dst_reduce_len,
+                axi_id:         req_i.opt.axi_id,
+                src_axi_opt:    req_i.opt.src,
+                dst_axi_opt:    req_i.opt.dst,
+                super_last:     req_i.opt.last
+            };
+            // determine shift amount
+            if (CombinedShifter) begin
+                opt_tf_d.read_shift  = req_i.src_addr[OffsetWidth-1:0] -
+                                       req_i.dst_addr[OffsetWidth-1:0];
+                opt_tf_d.write_shift = '0;
+            end else begin
+                opt_tf_d.read_shift  =   req_i.src_addr[OffsetWidth-1:0];
+                opt_tf_d.write_shift = - req_i.dst_addr[OffsetWidth-1:0];
+            end
+        end
+    end
+
+
+    //--------------------------------------
+    // Connect outputs
+    //--------------------------------------
+
+    // Read meta channel
+    always_comb begin
+        r_req_o.ar_req.obi.a_chan = '{
+            addr: { r_tf_q.addr[AddrWidth-1:OffsetWidth], {{OffsetWidth}{1'b0}} },
+            be: '1,
+            we: 1'b0,
+            wdata: '0,
+            aid: opt_tf_q.axi_id,
+            a_optional: '0
+        };
+    end
+
+    // assign the signals needed to set-up the read data path
+    assign r_req_o.r_dp_req = '{
+        src_protocol: opt_tf_q.src_protocol,
+        offset:       r_addr_offset,
+        tailer:       OffsetWidth'(r_num_bytes + r_addr_offset),
+        shift:        opt_tf_q.read_shift,
+        decouple_aw:  opt_tf_q.decouple_aw,
+        is_single:    r_num_bytes <= StrbWidth
+    };
+
+    // Write meta channel and data path
+    always_comb begin
+        w_req_o.aw_req.axi.aw_chan = '{
+            id: opt_tf_q.axi_id,
+            addr: { w_tf_q.addr[AddrWidth-1:OffsetWidth], {{OffsetWidth}{1'b0}} },
+            len: ((w_num_bytes + w_addr_offset - 'd1) >> OffsetWidth),
+            size: axi_pkg::size_t'(OffsetWidth),
+            burst: opt_tf_q.dst_axi_opt.burst,
+            lock: opt_tf_q.dst_axi_opt.lock,
+            cache: opt_tf_q.dst_axi_opt.cache,
+            prot: opt_tf_q.dst_axi_opt.prot,
+            qos: opt_tf_q.dst_axi_opt.qos,
+            region: opt_tf_q.dst_axi_opt.region,
+            user: '0,
+            atop: '0
+        };
+        w_req_o.w_dp_req = '{
+            dst_protocol: opt_tf_q.dst_protocol,
+            offset: w_addr_offset,
+            tailer: OffsetWidth'(w_num_bytes + w_addr_offset),
+            shift: opt_tf_q.write_shift,
+            num_beats: w_req_o.aw_req.axi.aw_chan.len,
+            is_single: w_req_o.aw_req.axi.aw_chan.len == '0
+        };
+        
+    end
+
+    // last burst in generic 1D transfer?
+    assign w_req_o.last = w_done;
+
+    // last burst indicated by midend
+    assign w_req_o.super_last = opt_tf_q.super_last;
+
+    // assign aw decouple flag
+    assign w_req_o.decouple_aw = opt_tf_q.decouple_aw;
+
+    // busy output
+    assign r_busy_o = r_tf_q.valid;
+    assign w_busy_o = w_tf_q.valid;
+
+
+    //--------------------------------------
+    // Flow Control
+    //--------------------------------------
+    // only advance to next state if:
+    // * rw_coupled: both machines advance
+    // * rw_decoupled: either machine advances
+
+    always_comb begin : proc_legalizer_flow_control
+        if ( opt_tf_q.decouple_rw
+            || (opt_tf_q.src_protocol inside { idma_pkg::OBI })) begin
+            r_tf_ena  = (r_ready_i & !flush_i) | kill_i;
+            w_tf_ena  = (w_ready_i & !flush_i) | kill_i;
+
+            r_valid_o = r_tf_q.valid & r_ready_i & !flush_i;
+            w_valid_o = w_tf_q.valid & w_ready_i & !flush_i;
+        end else begin
+            r_tf_ena  = (r_ready_i & w_ready_i & !flush_i) | kill_i;
+            w_tf_ena  = (r_ready_i & w_ready_i & !flush_i) | kill_i;
+
+            r_valid_o = r_tf_q.valid & w_ready_i & r_ready_i & !flush_i;
+            w_valid_o = w_tf_q.valid & r_ready_i & w_ready_i & !flush_i;
+        end
+    end
+
+    // load next idma request: if both machines are done!
+    assign ready_o = r_done & w_done & r_ready_i & w_ready_i & !flush_i;
+
+
+    //--------------------------------------
+    // State
+    //--------------------------------------
+    `FF (opt_tf_q, opt_tf_d,           '0, clk_i, rst_ni)
+    `FFL(r_tf_q,   r_tf_d,   r_tf_ena, '0, clk_i, rst_ni)
+    `FFL(w_tf_q,   w_tf_d,   w_tf_ena, '0, clk_i, rst_ni)
+
+
+    //--------------------------------------
+    // Assertions
+    //--------------------------------------
+    // only support the decomposition of incremental bursts
+    `ASSERT_NEVER(OnlyIncrementalBurstsSRC, (ready_o & valid_i &
+                  req_i.opt.src.burst != axi_pkg::BURST_INCR), clk_i, !rst_ni)
+    `ASSERT_NEVER(OnlyIncrementalBurstsDST, (ready_o & valid_i &
+                  req_i.opt.dst.burst != axi_pkg::BURST_INCR), clk_i, !rst_ni)
+
+endmodule
+
---
+++ target/rtl/idma_reg32_3d_top.sv
@@ -0,0 +1,193 @@
+// Copyright 2023 ETH Zurich and University of Bologna.
+// Solderpad Hardware License, Version 0.51, see LICENSE for details.
+// SPDX-License-Identifier: SHL-0.51
+
+// Authors:
+// - Michael Rogenmoser <michaero@iis.ee.ethz.ch>
+// - Thomas Benz <tbenz@iis.ee.ethz.ch>
+
+/// Description: Register-based front-end for iDMA
+module idma_reg32_3d #(
+  /// Number of configuration register ports
+  parameter int unsigned NumRegs        = 32'd1,
+  /// Number of streams (max 16)
+  parameter int unsigned NumStreams     = 32'd1,
+  /// Width of the transfer id (max 32-bit)
+  parameter int unsigned IdCounterWidth = 32'd32,
+  /// Dependent parameter: Stream Idx
+  parameter int unsigned StreamWidth    = cf_math_pkg::idx_width(NumStreams),
+  /// Register_interface request type
+  parameter type         reg_req_t      = logic,
+  /// Register_interface response type
+  parameter type         reg_rsp_t      = logic,
+  /// DMA 1d or ND burst request type
+  parameter type         dma_req_t      = logic,
+  /// Dependent type for IdCounterWidth
+  parameter type         cnt_width_t    = logic [IdCounterWidth-1:0],
+  /// Dependent type for StreamWidth
+  parameter type         stream_t       = logic [StreamWidth-1:0]
+) (
+  input  logic clk_i,
+  input  logic rst_ni,
+  /// Register interface control slave
+  input  reg_req_t [NumRegs-1:0] dma_ctrl_req_i,
+  output reg_rsp_t [NumRegs-1:0] dma_ctrl_rsp_o,
+  /// Request signals
+  output dma_req_t   dma_req_o,
+  output logic       req_valid_o,
+  input  logic       req_ready_i,
+  input  cnt_width_t next_id_i,
+  output stream_t    stream_idx_o,
+  /// Status signals
+  input  cnt_width_t           [NumStreams-1:0] done_id_i,
+  input  idma_pkg::idma_busy_t [NumStreams-1:0] busy_i,
+  input  logic                 [NumStreams-1:0] midend_busy_i
+);
+
+  /// Maximum number of streams is set to 16. It can be enlarged, but the register file
+  /// needs to be adapted too.
+  localparam int unsigned MaxNumStreams = 32'd16;
+
+  // register connections
+  idma_reg32_3d_reg_pkg::idma_reg32_3d_reg2hw_t [NumRegs-1:0] dma_reg2hw;
+  idma_reg32_3d_reg_pkg::idma_reg32_3d_hw2reg_t [NumRegs-1:0] dma_hw2reg;
+
+  // arbitration output
+  dma_req_t [NumRegs-1:0] arb_dma_req;
+  logic     [NumRegs-1:0] arb_valid;
+  logic     [NumRegs-1:0] arb_ready;
+
+  // register signals
+  reg_rsp_t [NumRegs-1:0] dma_ctrl_rsp;
+
+  always_comb begin
+      stream_idx_o = '0;
+      for (int r = 0; r < NumRegs; r++) begin
+          for (int c = 0; c < NumStreams; c++) begin
+              if (dma_reg2hw[r].next_id[c].re) begin
+                  stream_idx_o = c;
+              end
+          end
+      end
+  end
+
+  // generate the registers
+  for (genvar i = 0; i < NumRegs; i++) begin : gen_core_regs
+
+    idma_reg32_3d_reg_top #(
+      .reg_req_t ( reg_req_t ),
+      .reg_rsp_t ( reg_rsp_t )
+    ) i_idma_reg32_3d_reg_top (
+      .clk_i,
+      .rst_ni,
+      .reg_req_i ( dma_ctrl_req_i   [i] ),
+      .reg_rsp_o ( dma_ctrl_rsp     [i] ),
+      .reg2hw    ( dma_reg2hw       [i] ),
+      .hw2reg    ( dma_hw2reg       [i] ),
+      .devmode_i ( 1'b1                 )
+    );
+
+    logic read_happens;
+    // DMA backpressure
+    always_comb begin : proc_dma_backpressure
+      // ready signal
+      dma_ctrl_rsp_o[i]       = dma_ctrl_rsp[i];
+      dma_ctrl_rsp_o[i].ready = read_happens ? arb_ready[i] : dma_ctrl_rsp[i];
+    end
+
+    // valid signals
+
+    always_comb begin : proc_launch
+        read_happens = 1'b0;
+        for (int c = 0; c < NumStreams; c++) begin
+            read_happens |= dma_reg2hw[i].next_id[c].re;
+        end
+        arb_valid[i] = read_happens;
+    end
+
+    // assign request struct
+    always_comb begin : proc_hw_req_conv
+      // all fields are zero per default
+      arb_dma_req[i] = '0;
+
+      // address and length
+      arb_dma_req[i].burst_req.length   = dma_reg2hw[i].length_low.q;
+      arb_dma_req[i].burst_req.src_addr = dma_reg2hw[i].src_addr_low.q;
+      arb_dma_req[i].burst_req.dst_addr = dma_reg2hw[i].dst_addr_low.q;
+
+      // Protocols
+      arb_dma_req[i].burst_req.opt.src_protocol = idma_pkg::protocol_e'(dma_reg2hw[i].conf.src_protocol);
+      arb_dma_req[i].burst_req.opt.dst_protocol = idma_pkg::protocol_e'(dma_reg2hw[i].conf.dst_protocol);
+
+      // Current backend only supports incremental burst
+      arb_dma_req[i].burst_req.opt.src.burst = axi_pkg::BURST_INCR;
+      arb_dma_req[i].burst_req.opt.dst.burst = axi_pkg::BURST_INCR;
+        // this frontend currently does not support cache variations
+      arb_dma_req[i].burst_req.opt.src.cache = axi_pkg::CACHE_MODIFIABLE;
+      arb_dma_req[i].burst_req.opt.dst.cache = axi_pkg::CACHE_MODIFIABLE;
+
+      // Backend options
+      arb_dma_req[i].burst_req.opt.beo.decouple_aw    = dma_reg2hw[i].conf.decouple_aw.q;
+      arb_dma_req[i].burst_req.opt.beo.decouple_rw    = dma_reg2hw[i].conf.decouple_rw.q;
+      arb_dma_req[i].burst_req.opt.beo.src_max_llen   = dma_reg2hw[i].conf.src_max_llen.q;
+      arb_dma_req[i].burst_req.opt.beo.dst_max_llen   = dma_reg2hw[i].conf.dst_max_llen.q;
+      arb_dma_req[i].burst_req.opt.beo.src_reduce_len = dma_reg2hw[i].conf.src_reduce_len.q;
+      arb_dma_req[i].burst_req.opt.beo.dst_reduce_len = dma_reg2hw[i].conf.dst_reduce_len.q;
+
+      // ND connections
+      arb_dma_req[i].d_req[0].reps = dma_reg2hw[i].reps_2_low.q;
+      arb_dma_req[i].d_req[0].src_strides = dma_reg2hw[i].src_stride_2_low.q;
+      arb_dma_req[i].d_req[0].dst_strides = dma_reg2hw[i].dst_stride_2_low.q;
+      arb_dma_req[i].d_req[1].reps = dma_reg2hw[i].reps_3_low.q;
+      arb_dma_req[i].d_req[1].src_strides = dma_reg2hw[i].src_stride_3_low.q;
+      arb_dma_req[i].d_req[1].dst_strides = dma_reg2hw[i].dst_stride_3_low.q;
+
+      // Disable higher dimensions
+      if ( dma_reg2hw[i].conf.enable_nd.q == 0) begin
+        arb_dma_req[i].d_req[0].reps = '0;
+        arb_dma_req[i].d_req[1].reps = 'd1;
+      end
+      else if ( dma_reg2hw[i].conf.enable_nd.q == 1) begin
+        arb_dma_req[i].d_req[1].reps = 'd1;
+      end
+    end
+
+    // observational registers
+    for (genvar c = 0; c < NumStreams; c++) begin : gen_hw2reg_connections
+        assign dma_hw2reg[i].status[c]  = {midend_busy_i[c], busy_i[c]};
+        assign dma_hw2reg[i].next_id[c] = next_id_i;
+        assign dma_hw2reg[i].done_id[c] = done_id_i[c];
+    end
+
+    // tie-off unused channels
+    for (genvar c = NumStreams; c < MaxNumStreams; c++) begin : gen_hw2reg_unused
+        assign dma_hw2reg[i].status[c]  = '0;
+        assign dma_hw2reg[i].next_id[c] = '0;
+        assign dma_hw2reg[i].done_id[c] = '0;
+    end
+
+  end
+
+  // arbitration
+  rr_arb_tree #(
+    .NumIn     ( NumRegs   ),
+    .DataType  ( dma_req_t ),
+    .ExtPrio   ( 0         ),
+    .AxiVldRdy ( 1         ),
+    .LockIn    ( 1         )
+  ) i_rr_arb_tree (
+    .clk_i,
+    .rst_ni,
+    .flush_i ( 1'b0        ),
+    .rr_i    ( '0          ),
+    .req_i   ( arb_valid   ),
+    .gnt_o   ( arb_ready   ),
+    .data_i  ( arb_dma_req ),
+    .gnt_i   ( req_ready_i ),
+    .req_o   ( req_valid_o ),
+    .data_o  ( dma_req_o   ),
+    .idx_o   ( /* NC */    )
+  );
+
+endmodule
+
