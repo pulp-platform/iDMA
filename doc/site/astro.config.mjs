f675129155b71fa56bcf4fcc3beaf57b8ccd89ce
Daniel Keller <daniel.kellermartinez@csem.ch>
2026-06-11 10:23:48 +0200

    docs: Add license headers to Starlight site config files

--- doc/site/astro.config.mjs
+++ doc/site/astro.config.mjs
@@ -1,3 +1,10 @@
+// Copyright 2025 ETH Zurich and University of Bologna.
+// Solderpad Hardware License, Version 0.51, see LICENSE for details.
+// SPDX-License-Identifier: SHL-0.51
+
+// Authors:
+// - Daniel Keller <dankeller@iis.ee.ethz.ch>
+
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
--- doc/site/src/content.config.ts
+++ doc/site/src/content.config.ts
@@ -1,3 +1,10 @@
+// Copyright 2025 ETH Zurich and University of Bologna.
+// Solderpad Hardware License, Version 0.51, see LICENSE for details.
+// SPDX-License-Identifier: SHL-0.51
+
+// Authors:
+// - Daniel Keller <dankeller@iis.ee.ethz.ch>
+
 import { defineCollection } from 'astro:content';
 import { docsSchema } from '@astrojs/starlight/schema';
 
